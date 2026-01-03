4fb3aa474996d49a5d60cfb991773e4983a11eac
Mohit Singh <137917793+Mohito09@users.noreply.github.com>
2026-01-03 15:32:17 +0530

    Add root route to confirm backend is running

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
